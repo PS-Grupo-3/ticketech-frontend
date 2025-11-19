fdf0b1e6f9a01b3ca17ed8f3573b79bd392d860c
LovisottoSantiago <santiagolovisotto@gmail.com>
2025-11-19 02:42:28 -0300

    Sidebar login funcionando

--- src/shared/components/Layout.tsx
+++ src/shared/components/Layout.tsx
@@ -1,16 +1,16 @@
+import { useState } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import LoginSidebar from "../../modules/auth/pages/LoginSB";
 
-export default function Layout({
-  children,
-  onUserClick
-}: {
-  children: React.ReactNode;
-  onUserClick: () => void;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <div className="min-h-screen flex flex-col bg-neutral-900 text-white">
-      <Navbar onUserClick={onUserClick} />
+      <Navbar onUserClick={() => setSidebarOpen(true)} />
+
+      <LoginSidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
 
       <main className="flex-1">
         {children}
