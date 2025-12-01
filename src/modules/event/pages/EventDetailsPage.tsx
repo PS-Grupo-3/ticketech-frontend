fea1a1c6080c30aacc3444789bc6a8a09c2f1940
LovisottoSantiago <santiagolovisotto@gmail.com>
2025-11-30 23:46:15 -0300

    event detail

--- src/modules/event/pages/EventDetailsPage.tsx
+++ src/modules/event/pages/EventDetailsPage.tsx
@@ -250,7 +250,7 @@ export default function EventDetailPage() {
           >
             <h2
               className="text-3xl font-bold mb-4"
-              style={{ color: textOnTheme }}
+              
             >
               Comprar entradas
             </h2>
